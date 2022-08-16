3ec5be2f83e237189f8098f1cd4a39873aa62a40
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-08-16 11:42:37 +0800

    added Image

--- lib/main.dart
+++ lib/main.dart
@@ -51,18 +51,18 @@ class _MyHomePageState extends State<MyHomePage> {
   //Transaction
 
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'New Shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Weekly Groceries',
+    //   amount: 16.53,
+    //   date: DateTime.now(),
+    // ),
   ];
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
